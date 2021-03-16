df80c22e99311df6b7ef417cd1e71fa8d29494c4
HAROS <harchenk0s@yandex.ru>
2021-03-16 17:31:14 +0700

    Add Sattolo algorithm

---
+++ Sattolo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algoritmes
+{
+    public static class Sattolo
+    {
+        
+        public static void Do<T>(IList<T> items)
+        {
+            Random rand = new Random();
+            for (int i = items.Count - 1; i > 1; i--)
+            {
+                int j = rand.Next(i);
+                var tmp = items[i];
+                items[i] = items[j];
+                items[j] = tmp;
+            }
+        }
+    }
+}
