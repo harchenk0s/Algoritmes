fe71598f30ba79e23f7fd582e40889b103e2e658
HAROS <harchenk0s@yandex.ru>
2021-03-17 20:28:04 +0700

    Create Binary Search (Cycle)

---
+++ BinarySearch.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace Algoritmes
+{
+    public static class BinarySearch
+    {
+        public static (bool, int) Do<T>(T[] sorted_values, int value, bool descending = false) where T : IComparable
+        {
+            int left = 0;
+            for (int right = sorted_values.Length, middle; left < right;)
+            {
+                if (sorted_values[left].CompareTo(value) == 0)
+                    return (true, left);
+                middle = left + (right - left) / 2;
+                var c = sorted_values[middle].CompareTo(value);
+                if (c == 0)
+                {
+                    if (middle == left + 1)
+                        return (true, middle);
+                    right = middle + 1;
+                }
+                if ((c < 0) == descending)
+                    right = middle;
+                else
+                    left = middle + 1;
+            }
+            return (false, left);
+        }
+
+    }
+}
