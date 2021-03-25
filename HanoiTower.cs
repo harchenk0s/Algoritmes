e849c5ee109e9565e932e22476712784293527bf
HAROS <harchenk0s@yandex.ru>
2021-03-25 17:29:39 +0700

    Add interface

--- HanoiTower.cs
+++ HanoiTower.cs
@@ -7,6 +7,7 @@ namespace Algoritmes
     {
         List<List<int>> tower = new List<List<int>>();
         int towerHeight;
+        IStrategy strategy;
 
         public HanoiTower(int TowerHeight)
         {
@@ -22,15 +23,22 @@ namespace Algoritmes
                 tower[0].Add(i);
             }
 
-            for (int i = 1; i < 3; i++)
-            {
-                for (int j = 1; j <= TowerHeight; j++)
-                {
-                    tower[i].Add(0);
-                }
-            }
+            //for (int i = 1; i < 3; i++)
+            //{
+            //    for (int j = 1; j <= TowerHeight; j++)
+            //    {
+            //        tower[i].Add(0);
+            //    }
+            //}
             ShowTowers();
         }
+
+
+        public void Solve(IStrategy strategy)
+        {
+
+        }
+
 
         void ShowTowers()
         {
@@ -42,19 +50,70 @@ namespace Algoritmes
             {
                 for (int j = 0; j < 3; j++)
                 {
-                    if(tower[j][i] == 0)
-                    {
-                        Console.Write("       |       ");
-                    }
-                    else
+                    //if(tower[j][i] == 0)
+                    //{
+                    //    Console.Write("       |       ");
+                    //}
+                    //else
+                    //{
+                    //    Console.Write($"       {tower[j][i]}       ");
+                    //}
+                    try
                     {
                         Console.Write($"       {tower[j][i]}       ");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.Write("       |       ");
                     }
                 }
                 Console.Write("\n");
             }
             Console.WriteLine("__________________________________________________________________");
+        }
+    }
 
+
+    public interface IStrategy
+    {
+        void Algorithm(List<List<int>> towers);
+
+        
+    }
+
+
+    class EvenNumberAlgorithm : IStrategy
+    {
+        public void Algorithm(List<List<int>> towers)
+        {
+
+            int countSteps = Convert.ToInt32(Math.Pow(2, towers[0].Count) - 1);
+
+            for (int i = 0; i < countSteps; i++)
+            {
+
+            }
+        }
+
+        void Swap(List<List<int>> towers, int posA, int posB)
+        {
+            if ()
+            {
+
+            }
+            else
+            {
+
+            }
+        }
+
+    }
+
+
+    class OddNumberAlgorithm : IStrategy
+    {
+        public void Algorithm(List<List<int>> towers)
+        {
 
         }
     }
